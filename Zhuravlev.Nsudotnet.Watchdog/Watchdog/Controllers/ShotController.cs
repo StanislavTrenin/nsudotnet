f610235fcf61e84d558049df3f735ef0ca4e04f4
Ivan Yaschukovskiy <mr.ivan.yasch@gmail.com>
2018-05-27 20:47:03 +0700

    fixed one problem

--- Zhuravlev.Nsudotnet.Watchdog/Watchdog/Controllers/ShotController.cs
+++ Zhuravlev.Nsudotnet.Watchdog/Watchdog/Controllers/ShotController.cs
@@ -12,11 +12,12 @@ namespace Watchdog.Controllers
         {
             var memoryStream = new MemoryStream();
             var response = new HttpResponseMessage();
-            var photo = PhotoCatcher.GetPhotoCatcher().GetPhoto();
+            var photo = PhotoCatcher.Instance.GetPhoto();
             photo.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
             memoryStream.Seek(0, SeekOrigin.Begin);
             response.Content = new StreamContent(memoryStream);
             response.Content.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
+            photo.Dispose();
             return response;
         }
     }
